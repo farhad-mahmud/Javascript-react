d7b27a7cc7734864436bc9c7fd0b2a589bbc0b73
Farhad Mahmud <rixonahmed@gmail.com>
2026-05-12 02:25:25 +0600

    deployed in production and a billion dollar saas project

--- react/increment.js
+++ react/increment.js
@@ -1,6 +1,8 @@
 const domContainer = document.querySelector("#root");
 
+
 const Increment = () =>{
+
 
       const [count, setCount] = React.useState(0);
       
@@ -8,14 +10,34 @@ const Increment = () =>{
         <div>
             <h1 id = "display" >{count}</h1>
                 <div>
-                    <button id = "button" onClick={ () => setCount(count + 1)}>
-                        
+                    <button id = "button" onClick={ 
+                      () => setCount(count + 1)
+                      }>
                       Increment +
                     </button>
+                     <button id = "button" onClick={ 
+                      () => setCount(count - 1)
+                      }>
+                      Decrement +
+                    </button>
+
+                    <button id = "button" onClick={ 
+                      () => setCount(count -count)
+                      }>
+                      reset 
+                    </button>
+
                 </div>
         </div>
       );
-    };
 
-ReactDOM.render(<Increment />, domContainer);
+  };
+
+
+ReactDOM.render(
+    <div className = "container">
+         <Increment />
+    </div>,
+    domContainer
+);
 
